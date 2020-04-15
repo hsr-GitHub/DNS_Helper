6ff055f1bf8f1fd2bf36df87ef2a5cebbb79b04a
Hao Siru <h_s_r@qq.com>
2020-04-15 15:41:30 +0800

    Dev Editor update 20200415144130

--- DNE.py
+++ DNE.py
@@ -9,23 +9,23 @@ filename = 'DN.list'
 
 
 def lookup(domain_name_list_filename):
-	domain_name_list_file = open(domain_name_list_filename, 'r')
-	for line in domain_name_list_file:
-		print(line, end='')
-	domain_name_list_file.close()
+    domain_name_list_file = open(domain_name_list_filename, 'r')
+    for line in domain_name_list_file:
+        print(line, end='')
+    domain_name_list_file.close()
 
 
 def lookup_line(domain_name_list_filename, target):
-	line_now=0
-	back=''
-	domain_name_list_file = open(domain_name_list_filename, 'r')
-	for line in domain_name_list_file:
-		if line_now==target:
-			back=line
-			break
-		line_now+=1
-	domain_name_list_file.close
-	return back
+    line_now = 0
+    back = ''
+    domain_name_list_file = open(domain_name_list_filename, 'r')
+    for line in domain_name_list_file:
+        if line_now == target:
+            back = line
+            break
+        line_now += 1
+    domain_name_list_file.close
+    return back
 
 
 # def replace(domain_name_list_filename, source, replaced):
@@ -43,47 +43,61 @@ def lookup_line(domain_name_list_filename, target):
 #     os.remove('{}.bak'.format(domain_name_list_filename))
 
 
-def edit():
-	pass
+def edit(domain_name_list_filename, line_target, string):
+    string += '\n'
+    line_num = 0
+    shutil.copy(domain_name_list_filename,
+                '{}.tmp'.format(domain_name_list_filename))
+    domain_name_list_file = open(
+        '{}.tmp'.format(domain_name_list_filename), 'r')
+    domain_name_list_file_edited = open(domain_name_list_filename, 'w')
+    for line in domain_name_list_file:
+        if line_num == line_target:
+            line = string
+        domain_name_list_file_edited.write(line)
+        line_num += 1
+    domain_name_list_file.close()
+    domain_name_list_file_edited.close()
+    os.remove('{}.tmp'.format(domain_name_list_filename))
 
 
 def add(domain_name_list_filename, domain_name, explain):
-	domain_name_list_file = open(domain_name_list_filename, 'a')
-	domain_name_list_file.write('{}#{}\n'.format(domain_name, explain))
-	domain_name_list_file.close()
+    domain_name_list_file = open(domain_name_list_filename, 'a')
+    domain_name_list_file.write('{}#{}\n'.format(domain_name, explain))
+    domain_name_list_file.close()
 
 
 def update():
-	# domain_name_list_file_url=''
+    # domain_name_list_file_url=''
 
-	# if not os.path.exists(domain_name_list_filename):
-	#     urllib.request.urlretrieve(domain_name_list_file_url,domain_name_list_filename)
+    # if not os.path.exists(domain_name_list_filename):
+    #     urllib.request.urlretrieve(domain_name_list_file_url,domain_name_list_filename)
 
-	# domain_name_list_file=open(domain_name_list_filename)
-	pass
+    # domain_name_list_file=open(domain_name_list_filename)
+    pass
 
 
 def find(domain_name_list_filename, string):
-	lines = []
-	line_now = 0
-	domain_name_list_file = open(domain_name_list_filename, 'r')
-	for line in domain_name_list_file:
-		if string in line:
-			lines.append(line_now)
-		line_now += 1
-	domain_name_list_file.close()
-	return (0 if (lines == 0) else lines)
+    lines = []
+    line_now = 0
+    domain_name_list_file = open(domain_name_list_filename, 'r')
+    for line in domain_name_list_file:
+        if string in line:
+            lines.append(line_now)
+        line_now += 1
+    domain_name_list_file.close()
+    return (0 if (lines == 0) else lines)
 
 
 if __name__ == "__main__":
-	try:
-		temp = input('域名列表文件名（Ctrl + C）以跳过：')
-		filename = temp
-		del temp
-	except:
-		print('\n已跳过，文件路径：{}'.format(filename))
-	while True:
-		mode = input("""\n\n------\n
+    try:
+        temp = input('域名列表文件名（Ctrl + C）以跳过：')
+        filename = temp
+        del temp
+    except:
+        print('\n已跳过，文件路径：{}'.format(filename))
+    while True:
+        mode = input("""\n\n------\n
 输入你要进行的操作：
 输入'l'用来查看；
 输入'e'用来编辑；
@@ -92,44 +106,48 @@ if __name__ == "__main__":
 输入'q'以退出。
 l/e/a/u/q>\
 """)
-		try:
-			if mode == 'l':
-				lookup(filename)
-			if mode == 'e':
-				while True:
-					temp=input('')
-					if temp=='^':break
-					string=temp
-					lines=find(filename,string)
-					for i in lines:
-						print('#{}:{}'.format(i,lookup_line(filename,i)))
-					temp=input('')
-					if temp=='^':break
-					line=temp
-					lenes[temp]
-					# temp = input("你想替换那一项？（'^'以退出）")
-					# if temp == '^':
-					#     break
-					# source = temp
-					# temp = input("你想替成什么？（'^'以不变）")
-					# if temp == '^':
-					#     continue
-					# replaced = temp
-					# edit(filename, source, replaced)
-			if mode == 'a':
-				while True:
-					temp = input("你想增加什么？（'^'以退出）")
-					if temp == '^':
-						break
-					domain_name = temp
-					temp = input("中文：（'^'以留空）")
-					if temp == '^':
-						explain = '<unkonw>'
-					explain = temp
-					add(filename, domain_name, explain)
-			if mode == 'u':
-				update()
-		except:
-			print('')
-		if mode == 'q':
-			exit()
+        try:
+            if mode == 'l':
+                lookup(filename)
+            if mode == 'e':
+                while True:
+                    temp = input('')
+                    if temp == '^':
+                        break
+                    string = temp
+                    lines = find(filename, string)
+                    for i in lines:
+                        print('#{}:   {}'.format(
+                            i, lookup_line(filename, i)), end='')
+                    temp = input('')
+                    if temp == '^':
+                        break
+                    line = int(temp)
+                    string = input('')
+                    edit(filename, line, string)
+                    # temp = input("你想替换那一项？（'^'以退出）")
+                    # if temp == '^':
+                    #     break
+                    # source = temp
+                    # temp = input("你想替成什么？（'^'以不变）")
+                    # if temp == '^':
+                    #     continue
+                    # replaced = temp
+                    # edit(filename, source, replaced)
+            if mode == 'a':
+                while True:
+                    temp = input("你想增加什么？（'^'以退出）")
+                    if temp == '^':
+                        break
+                    domain_name = temp
+                    temp = input("中文：（'^'以留空）")
+                    if temp == '^':
+                        explain = '<unkonw>'
+                    explain = temp
+                    add(filename, domain_name, explain)
+            if mode == 'u':
+                update()
+        except:
+            print('')
+        if mode == 'q':
+            exit()
